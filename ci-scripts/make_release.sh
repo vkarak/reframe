c639ceae1fb9779548459680300dc49b9f22f5e6
Vasileios Karakasis <vkarak@gmail.com>
2026-08-25 23:42:27 +0200

    Fix automation after migrating to uv
    Also:
    - Add note about uv bug mishandling marker-based default dependencies.
    
    Signed-off-by: Vasileios Karakasis <vkarak@gmail.com>

--- ci-scripts/make_release.sh
+++ ci-scripts/make_release.sh
@@ -6,13 +6,13 @@ usage()
 {
     echo "Usage: $0 VERSION"
     echo "  Environment:"
-    echo "    - GH_DEPLOY_CREDENTIALS=<user>:<token>"
+    echo "    - GITHUB_CREDENTIALS=<user>:<token>"
 }
 
 _onerror()
 {
     exitcode=$?
-    echo "$0: ReFrame deployment failed!"
+    echo "$0: ReFrame release failed!"
     echo "$0: command \`$BASH_COMMAND' failed (exit code: $exitcode)"
     cd $oldpwd
     exit $exitcode
@@ -27,33 +27,31 @@ if [ -z $version ]; then
     exit 1
 fi
 
-py_minor_version=$(python3 -c 'import sys; print(sys.version_info.minor)')
-if [ $py_minor_version -lt 5 ]; then
-    echo "$0: deployment script requires Python>=3.5" >&2
+if [ ! uv --version >/dev/null 2>&1 ]; then
+    echo "$0: uv is not installed" >&2
     exit 1
 fi
 
-if [ -z "$GH_DEPLOY_CREDENTIALS" ]; then
+if [ -z "$GITHUB_CREDENTIALS" ]; then
     _gh_creds_prefix=""
 else
-    _gh_creds_prefix="${GH_DEPLOY_CREDENTIALS}@"
+    _gh_creds_prefix="${GITHUB_CREDENTIALS}@"
 fi
 
 
 tmpdir=$(mktemp -d)
-echo "Deploying ReFrame version $version ..."
+echo "Releasing ReFrame version $version ..."
 echo "Working directory: $tmpdir ..."
 cd $tmpdir
 git clone --branch master https://${_gh_creds_prefix}github.com/reframe-hpc/reframe.git
 cd reframe
-./bootstrap.sh
-found_version=$(./bin/reframe -V | sed -e 's/\(.*\)\+.*/\1/g')
+found_version=$(uv run reframe -V | sed -e 's/\(.*\)\+.*/\1/g')
 if [ $found_version != $version ]; then
     echo "$0: version mismatch: found $found_version, but required $version" >&2
     exit 1
 fi
 
-./test_reframe.py
+uv run ./test_reframe.py
 git tag -a v$version -m "ReFrame $version"
 git push origin --tags
 
